37d1d889c15b003e0b267bd159c50d0179714bcb
creohome <creohome@yandex.ru>
2024-05-16 01:31:46 +0500

    Update lab1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\UserController;
+ 
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::get('/info/server', [UserController::class, 'phpmyinfo'])->name('server');
+
+Route::get('/info/client', [UserController::class, 'clientip']);
+
+Route::get('/info/database', [UserController::class, 'dtb']);
